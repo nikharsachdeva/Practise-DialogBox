4f629cec0df23f406e9a8775c2308805cf91db7e
Nikhar Sachdeva <sachdevanikhar.work@gmail.com>
2019-05-30 02:40:56 +0530

    Code to add dialog box in android.

---
+++ app/src/main/java/com/example/android/dialogbox/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.dialogbox;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure!?")
+                .setMessage("Do you really want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Yes Selected",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"No Selected",Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+    }
+}
